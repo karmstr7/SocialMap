2171fe35adfc21ea4aaf79b4ae4a40205c6c93ac
Keir Armstrong <karmstr7@uoregon.edu>
2018-05-27 21:01:51 -0700

    save before pull

--- frontend/SocialMap/app/src/main/java/com/example/quinnm/socialmap/MainActivity.java
+++ frontend/SocialMap/app/src/main/java/com/example/quinnm/socialmap/MainActivity.java
@@ -1,54 +1,72 @@
 package com.example.quinnm.socialmap;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
-import com.mapbox.geojson.Point;
+import com.example.quinnm.socialmap.api.model.User;
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-//import com.mapbox.services.commons.geojson;
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
-import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.maps.MapView;
 
-import java.util.ArrayList;
-import java.util.List;
-//import com.mapbox.services.commons.geojson.FeatureCollection;
-//import com.mapbox.services.commons.geojson.Point;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
+    private boolean toolbarVisible = false;
+    private ImageButton _newMessageButton, _viewFriendsButton, _viewMyProfileButton;
+
+    private static final User DEFAULT_USER = new User("root","root");
+
+    private MapView mapView;
+    private MapboxMap mapboxMap;
     private static final String MARKER_SOURCE = "markers-source";
     private static final String MARKER_STYLE_LAYER = "markers-style-layer";
     private static final String MARKER_IMAGE = "custom-marker";
 
-    private MapView mapView;
-    private MapboxMap mapboxMap;
+    private static final int REQUEST_FINE_LOCATION_PERMISSION = 1;
+    private LocationManager locationManager;
+    private String locationProvider;
+    private Location lastKnownLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, "pk.eyJ1IjoicXVpbm5taWwiLCJhIjoiY2poNndlc2NuMDEyODJwcGd4OWw5d2M0YyJ9.ld7OWGt932HW0ebcv8CZFw");
-
         setContentView(R.layout.activity_main);
 
+        // get toolbar buttons
+        _newMessageButton = findViewById(R.id.btn_new_message);
+        _viewFriendsButton = findViewById(R.id.btn_view_friends);
+        _viewMyProfileButton = findViewById(R.id.btn_view_my_profile);
 
-        mapView = (MapView) findViewById(R.id.mapView);
+        Mapbox.getInstance(this, getString(R.string.mapbox_token));
+        mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
-
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 MainActivity.this.mapboxMap = mapboxMap;
+
+                mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng point) {
+                        setToolbarVisibility();
+                    }
+                });
 
                 //        Image: an image is loaded an added to the map
                 //        Bitmap icon = BitmapFactory.decodeResource(
@@ -56,8 +74,10 @@ public class MainActivity extends AppCompatActivity{
                 //        mapboxMap.addImage(MARKER_IMAGE, icon);
                 ////        addMarkers();
 
+                getMessages();
+
                 mapboxMap.addMarker(new MarkerOptions()
-//                        These coords aren't accurate
+                        // These coords aren't accurate
                         .position(new LatLng(44.44, -123.07))
                         .title("University of Oregon")
                         .snippet("Eugene, Oregon"));
@@ -68,9 +88,79 @@ public class MainActivity extends AppCompatActivity{
                         .snippet("Sandia Crest"));
             }
         });
+
+        _newMessageButton.setOnClickListener(
+                (View v) -> showNewMessageDialog()
+        );
+
+        _viewFriendsButton.setOnClickListener(
+                (View v) -> showFriendsListDialog()
+        );
+
+        _viewMyProfileButton.setOnClickListener(
+                (View v) -> showMyProfileDialog()
+        );
     }
 
-//    private void addMarkers(){
+    public void setToolbarVisibility() {
+        if (toolbarVisible) {
+            _newMessageButton.setVisibility(View.INVISIBLE);
+            _viewFriendsButton.setVisibility(View.INVISIBLE);
+            _viewMyProfileButton.setVisibility(View.INVISIBLE);
+        }
+        else {
+            _newMessageButton.setVisibility(View.VISIBLE);
+            _viewFriendsButton.setVisibility(View.VISIBLE);
+            _viewMyProfileButton.setVisibility(View.VISIBLE);
+        }
+        toolbarVisible = !toolbarVisible;
+    }
+
+    public void showNewMessageDialog() {
+        Toast.makeText(MainActivity.this, "Write a new message!", Toast.LENGTH_LONG).show();
+    }
+
+    public void showFriendsListDialog() {
+        Toast.makeText(MainActivity.this, "friends list", Toast.LENGTH_LONG).show();
+    }
+
+    public void showMyProfileDialog() {
+        Toast.makeText(MainActivity.this, "your info", Toast.LENGTH_LONG).show();
+    }
+
+    public void getMessages() {
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("localhost:8000/socialmap/api/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        MessageClient client = retrofit.create(MessageClient.class);
+        Call<Message> call = client.getMessages(DEFAULT_USER);
+
+        call.enqueue(new Callback<Message>() {
+            @Override
+            public void onResponse(Call<Message> call, Response<Message> response) {
+                Log.d(TAG,"Message response" + response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<Message> call, Throwable t) {
+                Toast.makeText(getBaseContext(), t.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+//msg=
+//    {
+//        "token": token,    # Type: string
+//        "username": username,
+//        "data":  data,    # Object containing data
+//        "body": body    # Type: string
+//    }
+
+
+    //    private void addMarkers(){
 //        List<Feature> features = new ArrayList<>();
 //        /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
 ////        ^ from Mapbox SDK
@@ -87,7 +177,6 @@ public class MainActivity extends AppCompatActivity{
 //                        PropertyFactory.iconImage(MARKER_IMAGE));
 //            mapboxMap.addLayer(markerStyleLayer);
 //    }
-
     @Override
     public void onStart() {
         super.onStart();
@@ -129,5 +218,4 @@ public class MainActivity extends AppCompatActivity{
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
-
-    }
+}
