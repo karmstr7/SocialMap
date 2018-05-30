8e0deac7451f5dd73ad5577896b45f1092e1920f
Keir Armstrong <karmstr7@users.noreply.github.com>
2018-05-30 13:33:15 -0700

    Update ViewFriendsActivity.java

--- frontend/SocialMap/app/src/main/java/com/example/quinnm/socialmap/ViewFriendsActivity.java
+++ frontend/SocialMap/app/src/main/java/com/example/quinnm/socialmap/ViewFriendsActivity.java
@@ -6,16 +6,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class ViewFriendsActivity extends AppCompatActivity {
-    private Button _mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_friends);
-        _mainButton = findViewById(R.id.btn_friends_to_main);
-
-        _mainButton.setOnClickListener(
-                (View v) -> finish()
-        );
     }
 }
